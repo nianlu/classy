0347b93f1c25689ac8e0860d6aed529ed02f298f
nianlu <S@Nianlus-iMac.home>
2019-06-16 23:00:56 -0400

    add bulma, node-sass

---
+++ client/src/container/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="navbar">
+        <a href="#">xxx</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
