a3e13ae2a63147c3a0ce831e6ab7f9c01af48655
nianlu <S@Nianlus-iMac.home>
2019-06-18 23:35:44 -0400

    testing tile box

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -3,23 +3,72 @@ import './App.sass';
 
 function App() {
   return (
-    <div className="container">
-      <nav className="navbar">
-        <button>aa</button>
-      </nav>
+    <div>
+      <div className="header">
+        <nav className="navbar">
+          <button>aa</button>
+        </nav>
+      </div>
 
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <div className="tile is-child box is-1">oo</div>
-          <div className="tile is-child box is-1">oo</div>
-          <div className="tile is-child box is-1">oo</div>
-          <div className="tile is-child box is-1">oo</div>
-          <div className="tile is-child box is-1">oo</div>
-          <div className="tile is-child box is-1">oo</div>
-          <div className="tile is-child box is-1">oo</div>
-          <div className="tile is-child box is-1">oo</div>
-          <div className="tile is-child box is-1">oo</div>
+      <div className="body">
+        <div className="tile is-ancestor" id="anc">
+          <div className="tile is-4 is-vertical" id="par">
+            <div className="tile" id="chi">
+              <div className="box">Top</div>
+              <div className="box">Top</div>
+              <div className="box">Top</div>
+            </div>
+            <div className="tile">
+              <div className="box">Bottom</div>
+            </div>
+          </div>
+          <div className="tile">
+            <div className="box">vertical</div>
+          </div>
         </div>
+
+        <div className="tile is-ancestor is-vertical" id="anc">
+          <div className="tile" id="par">
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+          </div>
+          <div className="tile" id="chi">
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+          </div>
+          <div className="tile" id="par">
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+            <div className="box">box</div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
