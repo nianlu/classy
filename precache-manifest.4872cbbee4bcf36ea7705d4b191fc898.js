44e945dd29c77968d936f859c4cb5dbb6ed6d8e4
nianlu <S@Nianlus-iMac.home>
2019-06-20 22:43:19 -0400

    Updates

---
+++ precache-manifest.4872cbbee4bcf36ea7705d4b191fc898.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aac664704df881ba923564dc8d0b6c7",
+    "url": "/classy/index.html"
+  },
+  {
+    "revision": "5c357482be7a41480b6f",
+    "url": "/classy/static/css/main.d99d9c9c.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/classy/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "5c357482be7a41480b6f",
+    "url": "/classy/static/js/main.e48d22f1.chunk.js"
+  },
+  {
+    "revision": "7452d8ed3b60f0ca0898",
+    "url": "/classy/static/js/runtime~main.e1d6a2f1.js"
+  }
+]);
