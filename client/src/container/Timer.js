9085d79b20bbb6ee17b2df771f9681dea6b7fb7c
Nianlu <nianlushu@gmail.com>
2019-07-12 01:01:59 -0400

    timer

--- client/src/container/Timer.js
+++ client/src/container/Timer.js
@@ -9,15 +9,11 @@ function Layout(props) {
   // const timer = setInterval(() => setTime((Date.now() - start)/1000), 100)
 
   const startTimer = () => {
-    console.log('start', timer)
     setTimer(setInterval(() => setTime((Date.now() - start)/1000), 100))
-    console.log('start', timer)
   }
 
   const stopTimer = () => {
-    console.log('stop', timer)
     setStart(Date.now())
-    console.log('stop', start)
     clearInterval(timer)
   }
 
@@ -26,21 +22,21 @@ function Layout(props) {
     setTime(0)
   }
 
-    return (
-        <div>
-          <nav className="panel">
-            <p className="panel-heading">
-              Timers {time}
-            </p>
-            <div className="panel-block">
-              <input type="text"></input>
-              <button onClick={startTimer}>start</button>
-              <button onClick={stopTimer}>stop</button>
-              <button onClick={cancelTimer}>cancel</button>
-            </div>
-          </nav>
-        </div>
-    )
+  return (
+      <div>
+        <nav className="panel">
+          <p className="panel-heading">
+            Timers {time}
+          </p>
+          <div className="panel-block">
+            <input type="text"></input>
+            <button onClick={startTimer}>start</button>
+            <button onClick={stopTimer}>stop</button>
+            <button onClick={cancelTimer}>cancel</button>
+          </div>
+        </nav>
+      </div>
+  )
 }
 
 export default Layout;
